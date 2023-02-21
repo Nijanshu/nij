fbe45190cae696062156b941e2eea4f8333e26f2
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2023-02-22 00:09:47 +0530

    Create vs.js
    Signed-off-by: Nijanshu <46223306+Nijanshu@users.noreply.github.com>

---
+++ vs.js
@@ -0,0 +1,7 @@
+let name=prompt("Enter your name")
+if(name!="Tin"){
+            document.write("Not for you")
+            msg.hidden=true
+            bdy.hidden=true
+            alert("Sorry")
+        }
